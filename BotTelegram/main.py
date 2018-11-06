5b6798b4ec9440b2727d01bcbaafbd552572ea4a
GnomeTV <38470242+GnomeTV@users.noreply.github.com>
2018-11-06 22:00:59 +0300

    Main file (version 1.3)

--- BotTelegram/main.py
+++ BotTelegram/main.py
@@ -1,28 +1,26 @@
 import telebot
 import const
-
-
 bot = telebot.TeleBot(const.token)
-# Basic KeyBoard
+# Стандартная клавиатура
 @bot.message_handler(commands=['start'])
 def handler_start(message):
     user_keyboard = telebot.types.ReplyKeyboardMarkup()
-    user_keyboard.row('My card', 'News')
-    user_keyboard.row('Cafes', 'Menu')
-    bot.send_message(message.from_user.id, "Aloha, doggy", reply_markup=user_keyboard)
-# KeyBoard for button My Card
+    user_keyboard.row('Моя карта', 'Новости')
+    user_keyboard.row('Кафе', 'Меню')
+    bot.send_message(message.from_user.id, "Алоха, бро😎", reply_markup=user_keyboard)
+# Клавиатура для кнопки (Моя карта)
 @bot.message_handler(content_types=['text'])
 def press_mycard(message):
-    if message.text == 'My card':
+    if message.text == 'Моя карта':
         mycard_keyboard = telebot.types.ReplyKeyboardMarkup()
-        mycard_keyboard.row('Enter number card', 'Back')
-        bot.send_message(message.from_user.id, "What do you want?", reply_markup=mycard_keyboard)
-
-@bot.message_handler(content_types=['text'])
-def message_back(message):
-    if message.text == 'Back':
+        mycard_keyboard.row('Сделать фото', 'Назад')
+        bot.send_message(message.from_user.id, "Выберите", reply_markup=mycard_keyboard)
+    if message.text == 'Назад':
         user_keyboard_back = telebot.types.ReplyKeyboardMarkup()
-        user_keyboard_back.row('My card', 'News')
-        user_keyboard_back.row('Cafes', 'Menu')
-        bot.send_message(message.from_user.id, "Aloha, doggy", reply_markup=user_keyboard_back)
-bot.polling(none_stop=True) 
+        user_keyboard_back.row('Моя карта', 'Новости')
+        user_keyboard_back.row('Кофейни', 'Меню')
+        bot.send_message(message.from_user.id, "Выберите", reply_markup=user_keyboard_back)
+
+
+
+bot.polling(none_stop=True)
