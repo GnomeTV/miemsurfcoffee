f21338f15595c1c6fec28034327db246b0002c82
GnomeTV <38470242+GnomeTV@users.noreply.github.com>
2018-11-09 21:32:49 +0300

    Main file (version 1.9)

--- BotTelegram/main.py
+++ BotTelegram/main.py
@@ -2,6 +2,8 @@ import telebot
 import const
 from telebot import types
 import requests
+from PIL import Image
+from pyzbar.pyzbar import decode
 bot = telebot.TeleBot(const.token)
 
 menu = ('''{:-^30}
@@ -152,10 +154,25 @@ def press_mycard(message):
         user_keyboard_back.row('Моя карта', 'Новости')
         user_keyboard_back.row('Кофейни', 'Меню')
         bot.send_message(message.from_user.id, '%s' %menu, reply_markup=user_keyboard_back)
+# Моя карта
+    fileName = str(str(message.chat.id) + '.jpg')
     if message.text == 'Моя карта':
         mycard_keyboard = telebot.types.ReplyKeyboardMarkup()
-        mycard_keyboard.row('Сделать фото', 'Назад')
+        mycard_keyboard.row('Узнать баланс', 'Назад')
         bot.send_message(message.from_user.id, "Выберите", reply_markup=mycard_keyboard)
+    if message.text == 'Узнать баланс':
+        bot.send_message(message.from_user.id, "Отправь фотку QR кода с обратной стороны карты")
+        @bot.message_handler(content_types=['photo'])
+        def handle_docs_photo(message):
+            file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
+            downloaded_file = bot.download_file(file_info.file_path)
+            src = '/Users/Ivan/Desktop/GitHub/miemsurfcoffee/telegramBot/' + fileName
+            with open(src, 'wb') as new_file:
+                new_file.write(downloaded_file)
+            bot.reply_to(message, "QRcode обрабатывается")
+            qr_code = decode(Image.open(fileName))
+            cardNum = str(qr_code).split()[0][16:-2]
+            bot.send_message(message.from_user.id, cardNum)
     if message.text == 'Назад':
         user_keyboard_back = telebot.types.ReplyKeyboardMarkup()
         user_keyboard_back.row('Моя карта', 'Новости')
